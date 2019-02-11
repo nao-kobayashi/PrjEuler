2a8fdb1970df732479392882a1c235f520af753b
M.Kobayashi <nao.kobayashi@nifty.com>
2019-02-11 16:25:03 +0900

    feat: solve problem18

--- src/main.rs
+++ src/main.rs
@@ -528,6 +528,45 @@ extern crate lazy_static;
 pub mod problem17;
 use problem17::solve;
 
+fn problem18() {
+    let data = vec![vec![75],
+                    vec![95,64],
+                    vec![17,47,82],
+                    vec![18,35,87,10],
+                    vec![20,04,82,47,65],
+                    vec![19,01,23,75,03,34],
+                    vec![88,02,77,73,07,63,67],
+                    vec![99,65,04,28,06,16,70,92],
+                    vec![41,41,26,56,83,40,80,70,33],
+                    vec![41,48,72,33,47,32,37,16,94,29],
+                    vec![53,71,44,65,25,43,91,52,97,51,14],
+                    vec![70,11,33,28,77,73,17,78,39,68,17,57],
+                    vec![91,71,52,38,17,14,91,43,58,50,27,29,48],
+                    vec![63,66,04,68,89,53,67,30,73,16,69,87,40,31],
+                    vec![04,62,98,27,23,09,70,98,73,93,38,53,60,04,23]
+    ];
+    let mut index = data.len() - 1;
+    let mut calculated = Vec::new();
+
+    while index > 0 {
+        let from_data = if calculated.len() == 0 {
+            data[index].clone()
+        } else {
+            calculated.clone()
+        };
+        calculated.clear();
+
+        for (i, n) in data[index - 1].iter().enumerate() {
+            calculated.push(n + max(from_data[i], from_data[i + 1]));
+        }
+
+        index -= 1;
+    }
+
+    println!("{:?}", calculated);
+
+}
+
 fn main() {
     let start = Instant::now();
     //problem1();
@@ -551,7 +590,8 @@ fn main() {
     //problem15(20);
     //problem16();
 
-    problem17::solve();
+    //problem17::solve();
+    problem18();
 
     let elapsed = start.elapsed();
     println!("Elapsed: {} ms", (elapsed.as_secs() * 1_000) + (elapsed.subsec_nanos() / 1_000_000) as u64);
