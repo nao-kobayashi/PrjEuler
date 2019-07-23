2680dfccd2f5d3dcd2571136f37bcd7b77b4cbd3
nao-kobayashi <nao.kobayashi@nifty.com>
2019-07-23 12:16:09 +0900

    refactor: problem83

--- problem83/src/main.rs
+++ problem83/src/main.rs
@@ -1,3 +1,5 @@
+use std::time::Instant;
+
 fn get_data() -> Vec<Vec<i32>> {
     let text_contents = std::fs::read_to_string("./p083_matrix.txt").unwrap();
     let all_lines = text_contents.split("\n");
@@ -16,7 +18,10 @@ fn get_data() -> Vec<Vec<i32>> {
     data
 }
 
+//425185
 fn main() {
+    let start = Instant::now();
+
     // let data = vec![
     //     vec![131,673,234,103,18],
     //     vec![201,96,342,965,150],
@@ -32,37 +37,54 @@ fn main() {
 
     for c in 0..max_col + 1 {
         for r in 0..max_row + 1 {
-            nodes.push(Node::new(c, r, max_col, max_row))
+            nodes.push(Node::new(r, c, max_row, max_col))
         }
     }
 
     nodes[0].cost = 0;
     loop {
+        //確定対象ノードを選択する。
+        //BinaryHeapを使ってもOK
         let mut done_node: i32 = -1;
         for (i, node) in nodes.iter().enumerate() {
             if node.done || node.cost < 0 {
                 continue;
             }
 
+            //一番小さいのを対象としたいのでbreakしてはダメ
             if done_node == -1 || node.cost < nodes[done_node as usize].cost {
                 done_node = i as i32;
             }
         }
+        //確定ノードのみになれば終了
         if done_node == -1 { break; }
 
-        nodes[done_node as usize].done = true;
-        for t in nodes.get(done_node as usize).unwrap().connect.clone() {
-            let cost = nodes[done_node as usize].cost + data[t.0][t.1];
+        //更新対象を集めておく
+        let node_index = done_node as usize;
+        nodes[node_index].done = true;
+        let mut update_source = Vec::new();
+        for t in nodes.get(node_index).unwrap().get_connect() {
+            let cost = nodes[node_index].cost + data[t.0][t.1];
             if let Some(index) = nodes.iter().position(|n| n.col == t.1 && n.row == t.0) {
                 if nodes[index].cost < 0 || cost < nodes[index].cost {
-                    nodes[index].cost = cost;
+                    update_source.push((index, cost, nodes[node_index].row, nodes[node_index].col));
                 }
             }
         }
+
+        //更新
+        for (index, cost, from_row, from_col) in update_source {
+            nodes[index].cost = cost;
+            nodes[index].from_row = from_row;
+            nodes[index].from_col = from_col;
+        }
     }
 
-    // nodes.iter().for_each(|node| println!("{:?}", node));
+    //nodes.iter().for_each(|node| println!("{:?}", node));
+    //スタート地点のコストを足す
     println!("{:?}", nodes[nodes.len() - 1].cost + data[0][0]);
+    let elapsed = start.elapsed();
+    println!("Elapsed: {} ms", (elapsed.as_secs() * 1_000) + (elapsed.subsec_nanos() / 1_000_000) as u64);
 }
 
 #[derive(Debug)]
@@ -70,12 +92,14 @@ struct Node {
     pub connect: Vec<(usize, usize)>,
     pub done: bool,
     pub cost: i32,
-    pub col: usize,
     pub row: usize,
+    pub col: usize,
+    pub from_row: usize,
+    pub from_col: usize,
 }
 
 impl Node {
-    pub fn new(col: usize, row: usize, max_col: usize, max_row: usize) -> Self {
+    pub fn new(row: usize, col: usize, max_row: usize, max_col: usize, ) -> Self {
         let mut connect = Vec::new();
         if col != max_col {
             connect.push((row, col + 1));
@@ -93,18 +117,23 @@ impl Node {
             connect.push((row - 1, col));
         }
 
+        //start地点への接続はなし
         if let Some(pos) = connect.iter().position(|t| t.0 == 0 && t.1 == 0) {
             connect.remove(pos);
         }
 
         Self {
             connect,
-            col,
             row,
+            col,
             done: false,
             cost: -1,
+            from_row: 0,
+            from_col: 0,
         }
     }
+
+    pub fn get_connect(&self) -> &[(usize, usize)] {
+        &self.connect
+    }
 }
-
-
